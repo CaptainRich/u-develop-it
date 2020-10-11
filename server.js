faeb1b90cebdec9b9131292b17ceb59d6e105681
RichA <rich_ay@sbcglobal.net>
2020-10-11 16:02:43 -0500

    Working on Routes.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 
-// Import the 'express' module
+// Import the 'express' module and the sqlite3 package
 
+const sqlite3 = require( 'sqlite3' ).verbose();
 const express = require( 'express' );
 const PORT    = process.env.PORT || 3001;
 const app     = express();
@@ -12,9 +13,80 @@ app.use(express.urlencoded( { extended: false } ));
 app.use(express.json());
 
 
+// Setup the connection to the SQL database. This creates an object, an
+// instance of the database file.
+
+const db = new sqlite3.Database( './db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
+
 // Specific routes go here, before the "catch-all 404" response.
 
 
+// The 'all' method runs the SQL command and executes the call-back, with 
+// matching data going to 'rows'.  'Rows' is the query response, an array of objects.
+// This will return all the candidates in the database.
+
+app.get('/api/candidates', (req, res) => {        // 'api' indicates this is an API endpoint
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// GET the data for a single candidate based on their ID
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+// Delete a candidate from the database, based on their ID
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+//   });
+
+
+// Create a record for a new candidate.
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+// ES5 function is necessary, not an arrow function, to use 'this'
+db.run(sql, params, function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
+});
 
 
 
@@ -26,7 +98,11 @@ app.use(express.json());
 
 
 
-// Start the express.js Server on port 3001
-app.listen( PORT, () => {
-    console.log( `Server running on port ${PORT}` );
+// Start the express.js Server on port 3001.  Need to make sure this starts after 
+// the database connection is established - put this in an event handler.
+
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
